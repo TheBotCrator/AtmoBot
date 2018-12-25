45f24d268cde0d4c1964cb93bd227fe5e3e7bcd1
Christopher <firedoglo1941@gmail.com>
2018-12-25 10:05:08 -0500

    Update BotRunner.js

--- BotRunner.js
+++ BotRunner.js
@@ -8,7 +8,7 @@ const Commando = require('discord.js-commando');
 global.Version = "0.0.6";
 global.Testing = false;
 global.Prefix = "ly!";
-global.Status = `Sector Greeting and Meeting. V: ${Version}`;
+global.Status = `Merry Christmas. V: ${Version}`;
 
 // Getting Bot Server Information
 global.Bot = new Commando.Client({
