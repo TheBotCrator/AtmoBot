bdfc054e633aa8d8db87c076dbe315b0bca5963c
Christopher <firedoglo1941@gmail.com>
2019-01-11 23:24:07 -0500

    Add files via upload

---
+++ commands/roles/start.js
@@ -0,0 +1,29 @@
+const Commando = require('discord.js-commando')
+const Discord = require('discord.js')
+
+class RStartCommand extends Commando.Command { 
+	constructor(client){
+		super(client, {
+			name: 'rstop',
+			group: 'role',
+			memberName: "rstop",
+			description: 'Will stop the Rainbow Color Process.'
+		});
+	}	
+	
+	async run(message, args){
+        if (message.author.equals(Bot.user)) return;
+		if (message.channel.type === "dm") return;
+		if (Testing === true) return;
+		
+		if (message.member.hasPermission('ADMINISTRATOR')) {
+			Stop.splice(Stop.indexOf(message.guild.id), 1);
+			return message.channel.send(":white_check_mark: Successfully Started Rainbow Roles.")
+		} else {
+			message.channel.send(":x: Missing Permissions 'ADMINISTRATOR'")
+			return;
+		}
+	}
+}
+
+module.exports = RStartCommand
---
+++ commands/roles/stop.js
@@ -0,0 +1,29 @@
+const Commando = require('discord.js-commando')
+const Discord = require('discord.js')
+
+class RStopCommand extends Commando.Command { 
+	constructor(client){
+		super(client, {
+			name: 'rstop',
+			group: 'role',
+			memberName: "rstop",
+			description: 'Will stop the Rainbow Color Process.'
+		});
+	}	
+	
+	async run(message, args){
+        if (message.author.equals(Bot.user)) return;
+		if (message.channel.type === "dm") return;
+		if (Testing === true) return;
+		
+		if (message.member.hasPermission('ADMINISTRATOR')) {
+			Stop.push(message.guild.id);
+			return message.channel.send(":white_check_mark: Successfully Stopped Rainbow Roles.")
+		} else {
+			message.channel.send(":x: Missing Permissions 'ADMINISTRATOR'")
+			return;
+		}
+	}
+}
+
+module.exports = RStopCommand
