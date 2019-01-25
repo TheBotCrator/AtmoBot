26ec51eb809f342065b1083d7b246010dee4a54a
Christopher <firedoglo1941@gmail.com>
2019-01-24 19:31:43 -0500

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -20,14 +20,19 @@ class BotCommand extends Commando.Command {
             .setAuthor("Lyaboo", "https://cdn.discordapp.com/avatars/513448452987027478/f63754a4f5e489cf7c03a0d797ca70b3.png?size=256")
             .setColor("6e00ff")
             .setFooter("Bot Developer: Pharaoh#1254", "https://cdn.discordapp.com/avatars/417835827700301836/808cb0b5521609d1f65d9938a6c790cb.png?size=256")
-            .addField("Bot Version", `${Version}`)
+            .setTimestamp()
+            .addField("Bot Library", "Discord.Js")
+	    .addField("Bot Commands", `${Prefix}help`)
+            .addField("Bot Prefix", `'${Prefix}'`)
+	    .addField("Bot Version", `${Version}`)
             .addBlankField(true);
-		let SecondEmbed = new Discord.RichEmbed()
-			.setAuthor("Lyaboo Dedications", "https://cdn.discordapp.com/avatars/513448452987027478/f63754a4f5e489cf7c03a0d797ca70b3.png?size=256")
-			.addField("Clickbait", "Owned by Forgetful#6666")
-			.addField("The Frosty Nation", "Owned by  #5243")
-			.setFooter("Want to see your Discord Added? DM Pharaoh#1254", "https://cdn.discordapp.com/avatars/417835827700301836/808cb0b5521609d1f65d9938a6c790cb.png?size=256")                     
-			.addBlankField(true);
+		
+	let SecondEmbed = new Discord.RichEmbed()
+		.setAuthor("Lyaboo Dedications", "https://cdn.discordapp.com/avatars/513448452987027478/f63754a4f5e489cf7c03a0d797ca70b3.png?size=256")
+		.addField("Clickbait", "Owned by Forgetful#6666")
+		.addField("The Frosty Nation", "Owned by  #5243")
+		.setFooter("Want to see your Discord Added? DM Pharaoh#1254", "https://cdn.discordapp.com/avatars/417835827700301836/808cb0b5521609d1f65d9938a6c790cb.png?size=256");    
+	   
 	   message.author.send(FirstEmbed); 
 	   message.author.send(SecondEmbed);
 	   message.channel.send(":white_check_mark: Please Check Direct Messages for More Information. If you didn't receive any messages, it's because your DM's are disabled.")
