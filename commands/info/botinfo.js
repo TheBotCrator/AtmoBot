4c007ecf3d79f03cd8496cebf8fdb8a66d86ea48
Christopher <firedoglo1941@gmail.com>
2019-01-24 19:26:55 -0500

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -17,21 +17,16 @@ class BotCommand extends Commando.Command {
 		if (Testing === true) return;
 		
         let FirstEmbed = new Discord.RichEmbed()
-            .setAuthor("Lyaboo", "https://cdn.discordapp.com/avatars/513448452987027478/0693b8c4738c0da560cce2ce0aa97802.png?size=256")
+            .setAuthor("Lyaboo", "https://cdn.discordapp.com/avatars/513448452987027478/f63754a4f5e489cf7c03a0d797ca70b3.png?size=256")
             .setColor("6e00ff")
-            .setFooter("Bot Developer: Pharaoh#6666", "https://cdn.discordapp.com/avatars/513448452987027478/0693b8c4738c0da560cce2ce0aa97802.png?size=256")
-            .setThumbnail("https://cdn.discordapp.com/avatars/513448452987027478/0693b8c4738c0da560cce2ce0aa97802.png?size=256")
-            .setTimestamp()
-            .addField("Bot Library", "Discord.Js")
-			.addField("Bot Commands", `${Prefix}help`)
-            .addField("Bot Prefix", `'${Prefix}'`)
+            .setFooter("Bot Developer: Pharaoh#1254", "https://cdn.discordapp.com/avatars/417835827700301836/808cb0b5521609d1f65d9938a6c790cb.png?size=256")
             .addField("Bot Version", `${Version}`)
             .addBlankField(true);
 		let SecondEmbed = new Discord.RichEmbed()
-			.setAuthor("Lyaboo Dedications", "https://cdn.discordapp.com/avatars/513448452987027478/0693b8c4738c0da560cce2ce0aa97802.png?size=256")
+			.setAuthor("Lyaboo Dedications", "https://cdn.discordapp.com/avatars/513448452987027478/f63754a4f5e489cf7c03a0d797ca70b3.png?size=256")
 			.addField("Clickbait", "Owned by Forgetful#6666")
 			.addField("The Frosty Nation", "Owned by  #5243")
-			.setFooter("Want to see your Discord Added? DM Pharaoh#6666", "https://cdn.discordapp.com/avatars/513448452987027478/0693b8c4738c0da560cce2ce0aa97802.png?size=256")
+			.setFooter("Want to see your Discord Added? DM Pharaoh#1254", "https://cdn.discordapp.com/avatars/417835827700301836/808cb0b5521609d1f65d9938a6c790cb.png?size=256")                     
 			.addBlankField(true);
 	   message.author.send(FirstEmbed); 
 	   message.author.send(SecondEmbed);
