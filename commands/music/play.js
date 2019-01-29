882e89e44b74995f617dc2cc0bb27a72e2ae46b5
Christopher <firedoglo1941@gmail.com>
2019-01-29 09:02:17 -0500

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -53,7 +53,7 @@ async function Play(Guild, Song) {
 	}
 	console.log(Queue.Queue);
 
-	const Dispatcher = Queue.Connection.playStream(YTDL(Song.url, { filter: "audioonly" }))
+	const Dispatcher = Queue.Connection.playStream(YTDL(Song.url, { filter: "audio" }))
 		.on('end', Reason => {
 			if (Reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
 			else console.log(Reason);
