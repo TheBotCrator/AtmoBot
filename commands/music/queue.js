466d839b335c2f816881fbfd815dc1e5821bfa13
Christopher <firedoglo1941@gmail.com>
2019-01-29 17:32:10 -0500

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -2,7 +2,7 @@ const Commando = require('discord.js-commando')
 const Discord = require('discord.js')
 const YTDL = require("ytdl-core")
 
-class StopCommand extends Commando.Command {
+class QueueCommand extends Commando.Command {
     constructor(client) {
         super(client, {
             name: 'queue',
@@ -20,14 +20,14 @@ class StopCommand extends Commando.Command {
 
         var Queue = Records[message.guild.id].Music;
 	    if (!Queue) return message.channel.send(':x: There is nothing playing.');
-		
+	    let Count = 0	
 	    let Embed = new Discord.RichEmbed()
 	    .setColor("#27037e")
 	    .setTitle(":musical_note: Song Queue :musical_note:")
-	    .setDescription(`${Queue.Queue.map(song => `:musical_note: ${song.title}`).join('\n')}`)
+	    .setDescription(`${Queue.Queue.map(song => `${++Count}. ${song.title}`).join('\n')}`)
 	    .addField("Now Playing", `${Queue.Queue[0].title}`);
 	    return message.channel.send("Current Songs Playing Now on Lyaboo", Embed)
     }
 }
 
-module.exports = StopCommand
+module.exports = QueueCommand
