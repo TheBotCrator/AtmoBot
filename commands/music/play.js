9bca39ec3a9a0e83d00d958791dbe1d5708075c2
Christopher <firedoglo1941@gmail.com>
2019-01-29 16:58:36 -0500

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -117,7 +117,7 @@ class PlayCommand extends Commando.Command {
 					let Embed = new Discord.RichEmbed()
 					.setColor("#27037e")
 					.setThumbnail(message.guild.iconURL)
-					.setDescription(`:musical_notes: ${Videos.map(Videos2 => `**${++Count} -** ${Videos2.title}`).join('\n')}`)
+					.setDescription(`:musical_note:/n${Videos.map(Videos2 => `**${++Count} -** ${Videos2.title}`).join('\n')}`)
 					.setTitle("Song Selection");
 					message.channel.send(`Please provide a value to select one of the search results ranging from 1-10.`, Embed)
 					try {
