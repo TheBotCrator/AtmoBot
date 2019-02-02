243850829dae6aed2d39a1a8145f5917e0339a2f
Christopher <firedoglo1941@gmail.com>
2019-02-02 05:40:43 -0500

    Add files via upload

--- structs/Schemas/suggestionSchema.js
+++ structs/Schemas/suggestionSchema.js
@@ -3,8 +3,8 @@ const Mongoose = Depends.Mongoose
 let SuggestionSchema = new Mongoose.Schema({
 	ServerID: String,
 	SuggestionsEnabled: Boolean,
-	SuggestionsChannel: Number,
-	RecordChannel: Number
+	SuggestionsChannel: String,
+	RecordChannel: String
 });
 
 module.exports = Mongoose.model("Suggestions", SuggestionSchema)
