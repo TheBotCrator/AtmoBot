632183428d80081dbc4c17e28c9ce83db20b61ac
Christopher <firedoglo1941@gmail.com>
2019-02-02 19:21:48 -0500

    Add files via upload

---
+++ commands/economy/profile.js
@@ -0,0 +1,50 @@
+const Commando = Depends.Commando
+const Discord = Depends.Discord
+const Mongoose = Depends.Mongoose
+
+class RankCommand extends Commando.Command { 
+	constructor(client){
+		super(client, {
+			name: 'rank',
+			group: 'economy',
+			memberName: "rank",
+			description: "To check a User's rank globally."
+		});
+	}	
+	
+	async run(message, args){
+        if (message.author.equals(Settings.Bot.user)) return;
+		if (message.channel.type === "dm") return;
+		if (Settings.Testing === true) return;
+		
+		let Args = message.content.split(" ")
+		Mongoose.connect(Settings.Connection + "\Level", {useNewUrlParser: true })
+		.catch(Error => {
+			console.log(Error)
+		})
+		
+		let User = message.mentions.members.first().id || message.author.id
+		
+		Settings.Schemas.Level.findOne({
+			UserId: User
+		}, (Error, Results) => {
+			if (Error) return console.log(Error);
+			if(!Results){
+				let Embed = new Discord.RichEmbed()
+				.setColor("6e00ff")
+				.setTitle(`Now Showing Profile of <@${User}>`)
+				.setDescription(`Data doesn't Exist.`)
+				return message.channel.send(":warning: User not found in Database!", Embed)
+			} else {
+				let Embed = new Discord.RichEmbed()
+				.setColor("6e00ff")
+				.setTitle(`Now Showing Profile of <@${User}>`)
+				.setDescription(`Level: ${Results.LevelNumber}\nExperience: ${Results.XPNumber}\nBalance: ${Results.MoneyNumber}`)
+				return message.channel.send(Embed)
+			}
+		})
+		
+	}
+}
+
+module.exports = RankCommand
