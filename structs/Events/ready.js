111a68fef19a6f3443d76ef7a2adc2cb2216a964
Christopher <firedoglo1941@gmail.com>
2019-02-02 22:07:02 -0500

    Add files via upload

--- structs/Events/ready.js
+++ structs/Events/ready.js
@@ -1,15 +1,3 @@
-async function Color() {
-    Depends.Timeout(Colors, (Color) => {
-        Settings.Bot.guilds.forEach((guild) => {
-            if (!Stop.includes(guild.id)) {
-                let role = guild.roles.find('name', 'Certified Customary');
-                if (role && role.editable)
-                    role.setColor(Color);
-            }
-        })
-    }, 1500).then(Color);
-}
-
 module.exports = (Bot, Member) => { 
 	console.log(`${Settings.Name} has loaded and is ready for Usage. Online at ${Settings.Bot.guilds.size}`);
     if (Settings.Testing === false) {
@@ -27,6 +15,5 @@ module.exports = (Bot, Member) => {
 				
 			}	
 		}	
-	})	
-    Color();
+	});
 }
