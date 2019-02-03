23e7dfdbb017a310cc5d22c8f2e7b53b3f6854f7
Christopher <firedoglo1941@gmail.com>
2019-02-02 22:25:25 -0500

    Add files via upload

--- structs/EconomyFunctions.js
+++ structs/EconomyFunctions.js
@@ -1,38 +1,36 @@
-module.exports = class Economoy {
-	addExperience(Message){
-		Depends.Mongoose.connect(Settings.Connection + "\Level", {useNewUrlParser: true })
+module.exports = (Bot, Message){
+	Depends.Mongoose.connect(Settings.Connection + "\Level", {useNewUrlParser: true })
 
-		Settings.Schemas.Level.findOne({
-			UserId: Message.author.id
-		}), (Error, Results) => {
-			let NewXP = Math.floor(Math.random() * 7) + 8;
-			if (!Results) {
-				let Level = new Settings.Schemas.Level({
-					UserId: Message.guild.id,
-					LevelNumber: 1,
-					XPNumber: NewXP,
-					MoneyNumber: 0
-				})
-				Level.save().then(Results => console.log(Results)).catch(Error => console.log(Error))
-				return;
-			} else {
-				let CurrentLevel = Results.LevelNumber;
-				let CurrentXP = Results.XPNumber;
-				let NextLevel = Results.LevelNumber * 300;
+	Settings.Schemas.Level.findOne({
+		UserId: Message.author.id
+	}), (Error, Results) => {
+		let NewXP = Math.floor(Math.random() * 7) + 8;
+		if (!Results) {
+			let Level = new Settings.Schemas.Level({
+				UserId: Message.guild.id,
+				LevelNumber: 1,
+				XPNumber: NewXP,
+				MoneyNumber: 0
+			})
+			Level.save().then(Results => console.log(Results)).catch(Error => console.log(Error))
+			return;
+		} else {
+			let CurrentLevel = Results.LevelNumber;
+			let CurrentXP = Results.XPNumber;
+			let NextLevel = Results.LevelNumber * 300;
 				
-				Results.XPNumber = CurrentXP + NewXP;
+			Results.XPNumber = CurrentXP + NewXP;
 
-				if(NextLevel <= Results.XPNumber){
-					Results.LevelNumber = Results.LevelNumber + 1;
-					let NewLevel = Results.LevelNumber
-					let Embed = new Discord.RichEmbed()
-					.setColor("6e00ff")
-					.setTitle("Congratulations!")
-					.setDescription(`You have leveled up to Level ${NewLevel}`);
-					Message.channel.send(`${Message.author}`, Embed).then(MSG => MSG.delete(10000))
-				}
-				Results.save().catch(Error => console.log(Error))
+			if(NextLevel <= Results.XPNumber){
+				Results.LevelNumber = Results.LevelNumber + 1;
+				let NewLevel = Results.LevelNumber
+				let Embed = new Discord.RichEmbed()
+				.setColor("6e00ff")
+				.setTitle("Congratulations!")
+				.setDescription(`You have leveled up to Level ${NewLevel}`);
+				Message.channel.send(`${Message.author}`, Embed).then(MSG => MSG.delete(10000))
 			}
+			Results.save().catch(Error => console.log(Error))
 		}
 	}
 }
